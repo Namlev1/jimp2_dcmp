153b4d1bdeda6a27a287b4a06896b16349b2f81c
war-duck <marcin.l.wardak@gmail.com>
2023-05-29 17:11:17 +0200

    Dodano opcje wyboru kompresora

--- app/decompressor/src/main/java/app/SceneController.java
+++ app/decompressor/src/main/java/app/SceneController.java
@@ -14,35 +14,66 @@ import java.io.File;
 import java.io.IOException;
 
 public class SceneController {
-    private File encodedFile, codeFile;
+    private File encodedFile, codeFile, uncompressedFile;
     @FXML private Label encodedFileChooserLabel;
+    @FXML private Button codeFileChooserButton;
+    @FXML private Button encodedFileChooserButton;
     @FXML private Label codeFileChooserLabel;
     @FXML private Label errorLabel;
     @FXML private VBox mainVBox;
     @FXML private VBox codeTableContent;
     @FXML private ScrollPane codeTableScrollPane;
     @FXML private VBox codeTableVBox;
+    @FXML private ComboBox modeSelector;
     @FXML
-    private void handleFileChooserButtonClicked(ActionEvent clickedButton) throws IOException {
+    private void handleFileChooserButtonClicked(ActionEvent clickedButton) {
+        if (!(clickedButton.getSource() instanceof Button))
+            return;
         FileChooser fileChooser = new FileChooser();
         fileChooser.setTitle("Wybierz skompresowany plik");
         fileChooser.setInitialDirectory(new java.io.File(System.getProperty("user.dir")+"/src/main/java/app")); // ustawiamy ścieżkę obecnego pliku jako domyślną ścieżkę
-        Object source = clickedButton.getSource();
-        if (source instanceof Button)
-            setExtensionFilter(fileChooser, ((Button)source)/*zwraca String z nazwą klikniętego przycisku*/);
-        else
-            throw new IOException("niepoprawnie wywołana metoda");
+        String sourceName = ((Button)clickedButton.getSource()).getId();
+        if (modeSelector.getValue().equals("Kompresor"))
+            sourceName = "compressor";
+        setExtensionFilter(fileChooser, sourceName/*zwraca String z nazwą klikniętego przycisku*/);
         File chosenFile = fileChooser.showOpenDialog(null);
         if (chosenFile == null)
             return;
-        setCurrentFile(chosenFile, (Button)source);
-        setLabelText(chosenFile.getName(), (Button)source);
+        setCurrentFile(chosenFile, sourceName);
+        setLabelText(chosenFile.getName(), sourceName);
         codeFileChooserLabel.getParent().requestLayout();
     }
+    @FXML void handleModeChanged(ActionEvent elementCalling) {
+        if (!(elementCalling.getSource() instanceof ComboBox))
+            return;
+        encodedFileChooserLabel.setText("");
+        codeFileChooserLabel.setText("");
+        switch ((String) modeSelector.getValue()) {
+            case "Dekompresor":
+                encodedFileChooserButton.setVisible(true);
+                encodedFileChooserButton.setVisible(true);
+                codeFileChooserButton.setText("Wybierz plik wejsciowy");
+
+                return;
+            case "Kompresor":
+                encodedFileChooserButton.setVisible(false);
+                encodedFileChooserButton.setVisible(false);
+                codeFileChooserButton.setText("Wybierz nieskompresowany plik wejściowy");
+        }
+    }
     @FXML
-    private void handleDecompressButtonPressed(ActionEvent elementCalling) throws IOException {
+    private void handleMainActionButtonClicked (ActionEvent elementCalling) throws IOException {
         if (!(elementCalling.getSource() instanceof Button))
             return;
+        switch ((String) modeSelector.getValue()) {
+            case "Dekompresor":
+                handleDecompressRequest();
+                return;
+            case "Kompresor":
+                handleCompressRequest();
+        }
+    }
+    private void handleDecompressRequest() throws IOException {
         if (encodedFile == null || codeFile == null)
         {
             mainVBox.setStyle("-fx-background-color: transparent");
@@ -54,6 +85,20 @@ public class SceneController {
         mainVBox.setStyle("-fx-background-color: #cccccc");
         DecompressorInitializer.initializeDecompressor(codeFile, encodedFile);
         displayCodeTable(DecompressorInitializer.dictionary);
+        mainVBox.setStyle("-fx-background-color: white");
+    }
+    private void handleCompressRequest() {
+        if (uncompressedFile == null)
+        {
+            mainVBox.setStyle("-fx-background-color: transparent");
+            errorLabel.setText("Wybierz plik");
+            errorLabel.setVisible(true);
+            return;
+        }
+        errorLabel.setVisible(false);
+        mainVBox.setStyle("-fx-background-color: #cccccc");
+        // tutaj wywolywana jest funkcja uruchamiajaca dekompresor, np initializeDecompressor(File uncompressedFile)
+        // displayCodeTable(DecompressorInitializer.dictionary); // można, nie trzeba, dodać opcję wyświetlania słownika po kompresji
         mainVBox.setStyle("-fx-background-color: white");
     }
 
@@ -92,11 +137,11 @@ public class SceneController {
         returnLabel.prefWidthProperty().bind(width);
         return returnLabel;
     }
-    private void setExtensionFilter(FileChooser fileChooser, Button associatedButton)
+    private void setExtensionFilter(FileChooser fileChooser, String associatedButton)
     {
         fileChooser.setSelectedExtensionFilter(null); // resetujemy poprzednio ustawione filtry
         ExtensionFilter chosenExtensionFilter;
-        switch (associatedButton.getId())
+        switch (associatedButton)
         {
             case "codeFileChooserButton":
                 chosenExtensionFilter = new ExtensionFilter("Słowniki kodów", "*.txt");
@@ -104,14 +149,17 @@ public class SceneController {
             case "encodedFileChooserButton":
                 chosenExtensionFilter = new ExtensionFilter("Zakodowane pliki", "*.bin");
                 break;
+            case "compressor":
+                chosenExtensionFilter = new ExtensionFilter("Wszystkie pliki", "*.*");
             default:
                 chosenExtensionFilter = new ExtensionFilter("Wszystkie pliki", "*.*");
         }
         fileChooser.getExtensionFilters().add(chosenExtensionFilter);
     }
-    private void setLabelText(String text, Button associatedButton){
-        switch (associatedButton.getId()){
+    private void setLabelText(String text, String associatedButton){
+        switch (associatedButton){
             case "codeFileChooserButton":
+            case "compressor":
                 codeFileChooserLabel.setText(text);
                 break;
             case "encodedFileChooserButton":
@@ -119,14 +167,16 @@ public class SceneController {
                 break;
         }
     }
-    private void setCurrentFile(File chosenFile, Button associatedButton){
-        switch (associatedButton.getId()){
+    private void setCurrentFile(File chosenFile, String associatedButton){
+        switch (associatedButton){
             case "codeFileChooserButton":
                 codeFile = chosenFile;
                 break;
             case "encodedFileChooserButton":
                 encodedFile = chosenFile;
                 break;
+            case "compressor":
+                uncompressedFile = chosenFile;
         }
     }
 }
